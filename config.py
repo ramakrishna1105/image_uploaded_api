0aeee63b3382dfd42bad9ff9ce41b7ea33e14b1a
ramakrishna1105 <kingsrama61@gmail.com>
2025-05-16 17:00:49 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+MYSQL_HOST = 'rama'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'ABCDE22@abcde'
+MYSQL_DB = 'image_upload_db'
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
